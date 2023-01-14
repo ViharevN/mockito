aa020df190ecde2d22fc822b4d5456f726528e5e
ViharevN <swix_off@mail.ru>
2023-01-14 13:38:50 +0300

    commit

---
+++ src/test/java/me/viharev/mockito/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.viharev.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
